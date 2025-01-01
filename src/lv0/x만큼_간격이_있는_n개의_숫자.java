b565a36faa873d9b6d41afd92743111d994bbcdc
dereck-jun <yjpark0805@naver.com>
2025-01-01 20:25:43 +0900

    25-01-01

---
+++ src/lv0/x만큼_간격이_있는_n개의_숫자.java
@@ -0,0 +1,24 @@
+package lv0;
+
+import java.util.Arrays;
+
+public class x만큼_간격이_있는_n개의_숫자 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        long[] result = solution.solution(-4, 1);
+        Arrays.stream(result).forEach(System.out::println);
+    }
+
+    static class Solution {
+        public long[] solution(int x, int n) {
+            long[] answer = new long[n];
+            answer[0] = x;
+
+            for (int i = 1; i < n; i++) {
+                answer[i] = answer[i - 1] + x;
+            }
+
+            return answer;
+        }
+    }
+}

e2490cb1cc2f1b1423cad0ef3a7de13075dd731d
dereck-jun <yjpark0805@naver.com>
2025-01-03 12:11:19 +0900

    25-01-03

---
+++ src/lv0/음양_더하기.java
@@ -0,0 +1,23 @@
+package lv0;
+
+public class 음양_더하기 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.solution(new int[]{4, 7, 12}, new boolean[]{true, false, true});
+        System.out.println(result);
+    }
+
+    static class Solution {
+        public int solution(int[] absolutes, boolean[] signs) {
+            int answer = 0;
+            for (int i = 0; i < absolutes.length; i++) {
+                if (signs[i]) {
+                    answer += absolutes[i];
+                } else {
+                    answer -= absolutes[i];
+                }
+            }
+            return answer;
+        }
+    }
+}

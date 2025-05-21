b919f56c37b4afd34fb7bcb3ec8f111aefdfdbfd
dereck-jun <yjpark0805@naver.com>
2025-05-21 19:07:40 +0900

    백준 - P10871

---
+++ src/baekjoon/P10871_X보다_작은_수.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class P10871_X보다_작은_수 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+
+		int[] A = new int[N];
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+
+		for (int num : A) {
+			if (num < X) {
+				bw.write(String.valueOf(num));
+				bw.newLine();
+			}
+		}
+		bw.flush();
+	}
+}

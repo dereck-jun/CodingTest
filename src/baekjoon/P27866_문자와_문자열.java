06f75e6b597e85c12b26925e7c74c0d3412e422a
dereck-jun <yjpark0805@naver.com>
2025-06-28 22:04:45 +0900

    백준 - P27866

--- src/baekjoon/P27866_문자와_문자열.java
+++ src/baekjoon/P27866_문자와_문자열.java
@@ -1,4 +1,19 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class P27866_문자와_문자열 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String input = br.readLine();
+		int idx = Integer.parseInt(br.readLine());
+
+		bw.write(input.charAt(idx - 1));
+		bw.flush();
+	}
 }

e87f413588012468ff0d8a2dcd3425e3e015f1af
dereck-jun <yjpark0805@naver.com>
2025-07-05 23:16:24 +0900

    백준 - P25083

---
+++ src/baekjoon/P25083_새싹.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class P25083_새싹 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write("         ,r'\"7");
+		bw.newLine();
+		bw.write("r`-_   ,'  ,/");
+		bw.newLine();
+		bw.write(" \\. \". L_r\'");
+		bw.newLine();
+		bw.write("   `~\\/");
+		bw.newLine();
+		bw.write("      |");
+		bw.newLine();
+		bw.write("      |");
+		bw.flush();
+		bw.close();
+	}
+}

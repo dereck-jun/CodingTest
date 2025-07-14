37e888b735bf4d643a7b8b0fefa1e2b7f6733dca
dereck-jun <yjpark0805@naver.com>
2025-07-15 00:01:47 +0900

    fix: 백준 - P2566

--- src/baekjoon/p2566_최댓값.java
+++ src/baekjoon/p2566_최댓값.java
@@ -9,24 +9,25 @@ public class p2566_최댓값 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 
 		int[][] arr = new int[9][9];
 		int max = 0;
-		String idx;
+		String idx = "";
 
 		for (int i = 0; i < 9; i++) {
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 9; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 				if (arr[i][j] > max) {
 					max = arr[i][j];
-					idx = String.valueOf(arr[i][j]);
+					idx = String.valueOf(i + " " + j);
 				}
 			}
 		}
 
 		System.out.println(max);
-		System.out.println();
+		System.out.println(idx);
 	}
 
 }

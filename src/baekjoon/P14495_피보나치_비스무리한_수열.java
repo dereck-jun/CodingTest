e6780382f4bcc62319c9a587a345b83166c8d857
dereck-jun <yjpark0805@naver.com>
2025-08-08 23:18:02 +0900

    백준 - P14495

---
+++ src/baekjoon/P14495_피보나치_비스무리한_수열.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P14495_피보나치_비스무리한_수열 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		long[] dp = new long[Math.max(4, n + 1)];
+
+		dp[1] = dp[2] = dp[3] = 1;
+
+		for (int i = 4; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 3];
+		}
+
+		System.out.println(dp[n]);
+	}
+
+}

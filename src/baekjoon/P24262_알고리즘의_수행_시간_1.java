6b7f29eb90610c4cfa52bb5cc3e4ef9a29fa1353
dereck-jun <116302666+dereck-jun@users.noreply.github.com>
2025-08-22 15:20:25 +0900

    백준 - P24262, P24263, P24264

---
+++ src/baekjoon/P24262_알고리즘의_수행_시간_1.java
@@ -0,0 +1,8 @@
+package baekjoon;
+
+public class P24262_알고리즘의_수행_시간_1 {
+	public static void main(String[] args) throws Exception {
+		System.out.println("1");
+		System.out.println("0");
+	}
+}
---
+++ src/baekjoon/P24263_알고리즘의_수행_시간_2.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P24263_알고리즘의_수행_시간_2 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println(br.readLine());
+		System.out.println("1");
+	}
+}

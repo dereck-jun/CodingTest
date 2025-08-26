ee6068ec1aa6e71a5811077c483ff747b2a55df0
dereck-jun <116302666+dereck-jun@users.noreply.github.com>
2025-08-26 23:47:26 +0900

    백준 - P24313

---
+++ src/baekjoon/P24313_점근적_표기_1.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P24313_점근적_표기_1 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int a1 = Integer.parseInt(st.nextToken());
+		int a0 = Integer.parseInt(st.nextToken());
+
+		int c = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
+
+		if (a1 * n + a0 <= c * n && a1 <= c) {
+			System.out.println("1");
+		} else {
+			System.out.println("0");
+		}
+	}
+}
